f99d01b4bc6e7f525828188bf0bc84c85be32655
musfiqur552608 <mushfiqur15-10872@diu.edu.bd>
2022-09-23 16:00:08 +0600

    get countries by component

---
+++ src/components/Country/Country.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Country.css'
+
+const Country = (props) => {
+    const {name, population, region, area} = props.country;
+    return (
+        <div className='country'>
+            <h2>Country Name: {name.common}</h2>
+            <p>Population: {population}</p>
+            <p><small>Region: {region}</small></p>
+            <p><small>Area: {area}</small></p>
+        </div>
+    );
+};
+
+export default Country;
